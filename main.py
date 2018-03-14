895c0e1a2c622bd889db817083cdfcfc4cb36975
Will Russell <whiskeyromeo06@gmail.com>
2018-03-14 09:59:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,8 @@
 main.py -- Udacity conference server-side Python App Engine
     HTTP controller handlers for memcache & task queue access
 
-$Id$
-
-created by wesc on 2014 may 24
-
 """
 
-__author__ = 'wesc+api@google.com (Wesley Chun)'
 
 import webapp2
 from google.appengine.api import app_identity
